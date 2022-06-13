6f650fc7580e5a7f9650ad839b6b796b62e642c6
Irfan Imaduddin <irfanimaduddin@gmail.com>
2022-06-13 22:53:15 +0700

    create trending_video.py

--- trending_video.py
+++ trending_video.py
@@ -10,7 +10,7 @@ url_trending_live = url_ytgaming + "/games"
 url_trending_video = url_ytgaming + "/trending"
 
 driver = webdriver.Firefox()
-driver.get(url_trending_live)
+driver.get(url_trending_video)
 
 print("Beres load page: {}".format(datetime.now()))
 
@@ -22,26 +22,32 @@ while i<10:
     i += 1
     print("Beres scroll ke-{}: {}".format(i, datetime.now()))
 
-
-resp_game_title = driver.find_elements(by=By.XPATH, value='//yt-formatted-string[@id="title" and @class="style-scope ytd-game-details-renderer"]')
-resp_game_views = driver.find_elements(by=By.XPATH, value='//yt-formatted-string[@id="live-viewers-count" and @class="style-scope ytd-game-details-renderer"]')
+resp_channel_names = driver.find_elements(by=By.XPATH, value='//a[@class="yt-simple-endpoint style-scope yt-formatted-string"]')
+resp_video_titles = driver.find_elements(by=By.XPATH, value='//a[@id="video-title" and @class="yt-simple-endpoint style-scope ytd-grid-video-renderer"]')
+resp_video_views = driver.find_elements(by=By.XPATH, value='//div[@id="metadata-line" and @class="style-scope ytd-grid-video-renderer"]')
 
 update_at = datetime.now() 
 new_array = []
 
-for i, (column1, column2) in enumerate(zip(resp_game_title, resp_game_views)):
+for i, (column1, column2, column3) in enumerate(zip(resp_channel_names, resp_video_titles, resp_video_views)):
     try:
-        game_title = column1.text
-        game_view = column2.find_element(by=By.TAG_NAME, value="span").text
-        if 'K' in game_view:
-            game_view = int(float(game_view[:-1])*1000)
+        channel_name = column1.text
+        video_title = column2.text
+        video_views = column3.find_element(by=By.TAG_NAME, value="span").text.split(" ")[0]
+        video_url = column2.get_attribute("href")
+
+        if 'M' in video_views:
+            video_views = int(float(video_views[:-1])*1000000)
+        elif 'K' in video_views:
+            video_views = int(float(video_views[:-1])*1000)
         else:
-            game_view = int(game_view)
-        new_dict = {"update_at": update_at.timestamp(), "game_title": game_title, "live_views": game_view}
+            video_views = int(video_views)
+
+        new_dict = {"update_at": update_at.timestamp(), "channel_name": channel_name, "video_title": video_title, "video_views": video_views, "video_url": video_url}
         new_array.append(new_dict)
     except:
         break
-
+    
 df = pd.DataFrame(new_array)
 df.to_csv('./output/output.csv', index=False)
 
